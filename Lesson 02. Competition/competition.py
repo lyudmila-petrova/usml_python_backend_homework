37bf9db720894e4609a60df36326243c8b9f08cf
ging74ru <ging74ru@gmail.com>
2018-10-02 14:04:18 +0300

    lesson 02

---
+++ Lesson 02. Competition/competition.py
@@ -0,0 +1,76 @@
+# Cпроектируйте объектную модель соревнования, вы должны учитывать разные спецификации
+# автомобилей, погодные условия, и то что в один момент времени может проходить
+# только одно соревнование, как итог, запустите гонку с разными автомобилями и
+# продемонстрируйте победителей)
+#
+# Требования к решению:
+#
+# Описать 3 класса:
+#     - Автомобиль
+#     - Погода
+#     - Соревнование
+# Выполнить требования к классу Автомобиль:
+#     - Зафиксировать спецификации автомобилей как атрибут класса
+# Выполнить требования к классу Погода:
+#     - Реализовать доступ к функции получения скорости ветра как к переменной экземпляра класса
+# Выполнить требования к классу Соревнование:
+#     - в качестве входных аргументов принимает 1 параметр — длина дистанции
+#     - не позволять создание более 1 экземпляра класса (обратите внимание на метод класса new)
+
+from random import randint
+
+
+class Car:
+    CAR_SPECS = {
+        'ferrary': {"max_speed": 340, "drag_coef": 0.324, "time_to_max": 26},
+        'bugatti': {"max_speed": 407, "drag_coef": 0.39, "time_to_max": 32},
+        'toyota': {"max_speed": 180, "drag_coef": 0.25, "time_to_max": 40},
+        'lada': {"max_speed": 180, "drag_coef": 0.32, "time_to_max": 56},
+        'sx4': {"max_speed": 180, "drag_coef": 0.33, "time_to_max": 44},
+    }
+
+
+class Weather:
+    def __init__(self, max_wind_speed=20):
+        self._max_wind_speed = max_wind_speed
+
+    @property
+    def wind_speed(self):
+        return randint(0, self._max_wind_speed)
+
+
+class Competition(object):
+    __instance = None
+
+    def __new__(cls, val):
+        if Competition.__instance is None:
+            Competition.__instance = object.__new__(cls)
+            Competition.__instance.val = val
+        return Competition.__instance
+
+    def __init__(self, distance, weather=None):
+        self.distance = distance
+        self.weather = weather or Weather()
+
+    def start(self, car_specs):
+        for name, car in car_specs.items():
+            time = 0
+
+            for distance in range(self.distance):
+                wind_speed = self.weather.wind_speed
+
+                if time == 0:
+                    speed = 1
+                else:
+                    speed = (time / car["time_to_max"]) * car['max_speed']
+                    if speed > wind_speed:
+                        speed -= (car["drag_coef"] * wind_speed)
+
+                time += float(1) / speed
+
+            print("Car <%s> result: %f" % (name, time))
+
+
+if __name__ == "__main__":
+    competition = Competition(10000)
+    competition.start(Car.CAR_SPECS)
