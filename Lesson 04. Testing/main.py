cd897d532a187ab58436e53a5bb3446c7fa32566
ging74ru <ging74ru@gmail.com>
2018-10-16 22:20:28 +0300

    lesson 04 created tests for function

---
+++ Lesson 04. Testing/main.py
@@ -0,0 +1,5 @@
+import unittest
+from tests import TestGCD
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ Lesson 04. Testing/tests.py
@@ -0,0 +1,33 @@
+import unittest
+from gcd import gcd
+
+__version__ = '0.1'
+
+
+class TestGCD(unittest.TestCase):
+    def test_zeros(self):
+        self.assertRaises(ValueError, gcd, 0, 0)
+
+    def test_fractional(self):  #
+        self.assertRaises(ArithmeticError, gcd, 2.5, 1)
+
+    def test_coprime_numbers(self):
+        cases = {
+            (5, 8): 1,
+            (16, 27): 1,
+            (11, 34): 1
+        }
+        for params, result in cases.items():
+            with self.subTest(case=params):
+                self.assertEqual(gcd(*params), result)
+
+    def test_negative(self):
+        cases = {
+            (1, -1): 1,
+            (-32, 0): 32,
+            (-64, -64): 64,
+            (6, -15): 3
+        }
+        for params, result in cases.items():
+            with self.subTest(case=params):
+                self.assertEqual(gcd(*params), result)
